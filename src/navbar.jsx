c9ad7ed0b70fe16d073c4c87215050af28106bd5
abrarfuad27 <abrarfuad51@gmail.com>
2023-07-10 00:05:35 +0600

    setting up project

---
+++ src/navbar.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import './styles.css'
+
+export default function NavBar(){
+    return (
+        <nav className="navbar">
+            <ul>
+                <li>Resume</li>
+            </ul>
+        </nav>
+    )
+}
