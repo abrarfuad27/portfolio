1a1bbd6ab0ef1c1ae108416d9ca43d7cbe7cbb90
abrarfuad27 <abrarfuad51@gmail.com>
2023-08-07 00:01:49 +0600

    added pomodragon

--- src/data.js
+++ src/data.js
@@ -1,9 +1,24 @@
 export default [
   {
+    image: "assets/pomo.png",
+    title: "Pomodragon",
+    description:
+      " Fast and Responsive Pomodoro Timer! It comes with a personalised activity log that spans the last 30 days to give you insights into your productivity patterns. A full stack application with the help of MongoDB,Express,React,Node along with libraries such as Chart.js, react-circle-timer, and JWT for authentication.",
+    url: "https://pomodragon.vercel.app/",
+  },
+  {
+    image: "assets/tenzies.png",
+    title: "Tenzies",
+    description:
+      "A number generator game in React.js. It has a built-in highscore system along with a timer. Play with friends to see who gets luckier!",
+    url: "https://tenzies-rho.vercel.app/",
+  },
+  ,
+  {
     image: "assets/asteroids.png",
     title: "Asteroids",
     description:
-      "The classic game Asteroids written in Python.  The game features smooth controls and realistic physics, providing an engaging and challenging gameplay experience. With the simplegui library used for the front-end, the game offers an intuitive graphical interface, making it accessible and enjoyable for players of all skill levels.",
+      "The classic game Asteroids in Python. With the simplegui library used for the front-end, the game offers an intuitive graphical interface, making it accessible and enjoyable for players.",
     url: "https://py2.codeskulptor.org/#user49_HkBGrNjZct_6.py",
   },
   {
@@ -12,12 +27,5 @@ export default [
     description:
       "Pong written in Python! The game features smooth movement, collision detection, and an interactive interface. Players can compete against each other or play against a computer-controlled opponent. Pong provides an entertaining and engaging experience that tests players' reflexes and strategic thinking.",
     url: "https://py2.codeskulptor.org/#user49_FClmURzpfg_12.py",
-  },
-  {
-    image: "assets/tenzies.png",
-    title: "Tenzies",
-    description:
-      "A number generator game in React.js. It has a built-in highscore system along with a timer. Play with friends to see who gets luckier!",
-    url: "https://tenzies-rho.vercel.app/",
   },
 ];
