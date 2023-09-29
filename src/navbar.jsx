8873cb89f74d5eccb17d9278a49a36b4204caeea
abrarfuad27 <abrarfuad51@gmail.com>
2023-09-29 12:57:50 -0400

    updated resume

--- src/navbar.jsx
+++ src/navbar.jsx
@@ -27,7 +27,7 @@ export default function NavBar() {
         </li>
         <li className="nav-item">
           <a
-            href="https://drive.google.com/file/d/1k23q8or5cJFe9w_bHtmvNw5VSXvx1oZQ/view?usp=sharing"
+            href="https://drive.google.com/file/d/1Hy6zPMC7GXM8Nz19R8H13WVch0dBosiY/view?usp=sharing"
             target="_blank"
           >
             Resume
