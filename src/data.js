0b3b2422567a68227df6e5c5b746b1bf1bae0489
abrarfuad27 <abrarfuad51@gmail.com>
2023-12-01 22:04:15 -0500

    added projects

--- src/data.js
+++ src/data.js
@@ -7,13 +7,27 @@ export default [
     url: "https://pomodragon.vercel.app/",
   },
   {
-    image: "assets/tenzies.png",
-    title: "Tenzies",
+    image: "assets/langsonic.png",
+    title: "LangSonic",
     description:
-      "A number generator game in React.js. It has a built-in highscore system along with a timer. Play with friends to see who gets luckier!",
-    url: "https://tenzies-rho.vercel.app/",
+      "A spoken language classifier, built on top of a Convolutional Neural Network which was trained with over 450000 audio files.",
+    url: "https://lit-mesa-88597-fe23f01307fc.herokuapp.com/",
   },
   ,
+  // {
+  //   image: "assets/tenzies.png",
+  //   title: "Tenzies",
+  //   description:
+  //     "A number generator game in React.js. It has a built-in highscore system along with a timer. Play with friends to see who gets luckier!",
+  //   url: "https://tenzies-rho.vercel.app/",
+  // },
+  {
+    image: "assets/penguinslay.gif",
+    title: "SignLingo",
+    description:
+      "An inclusive American Sign Language (ASL) learning tool that provides users with real-time feedback on the accuracy of their hand gestures during interactive quizzes, ensuring continuous progress tracking and enhanced learning experiences.",
+    url: "https://github.com/abrarfuad27/SignLingo",
+  },
   {
     image: "assets/asteroids.png",
     title: "Asteroids",
@@ -21,11 +35,11 @@ export default [
       "The classic game Asteroids in Python. With the simplegui library used for the front-end, the game offers an intuitive graphical interface, making it accessible and enjoyable for players.",
     url: "https://py2.codeskulptor.org/#user49_HkBGrNjZct_6.py",
   },
-  {
-    image: "assets/pong.png",
-    title: "Pong",
-    description:
-      "Pong written in Python! The game features smooth movement, collision detection, and an interactive interface. Players can compete against each other or play against a computer-controlled opponent. Pong provides an entertaining and engaging experience that tests players' reflexes and strategic thinking.",
-    url: "https://py2.codeskulptor.org/#user49_FClmURzpfg_12.py",
-  },
+  // {
+  //   image: "assets/pong.png",
+  //   title: "Pong",
+  //   description:
+  //     "Pong written in Python! The game features smooth movement, collision detection, and an interactive interface. Players can compete against each other or play against a computer-controlled opponent. Pong provides an entertaining and engaging experience that tests players' reflexes and strategic thinking.",
+  //   url: "https://py2.codeskulptor.org/#user49_FClmURzpfg_12.py",
+  // },
 ];
